7fe84a7012ca0d2a1c354e4f761f0169c3dd8a2a
Muhammad Saad <muhammadsaadicup@gmail.com>
2024-10-07 19:44:32 +0400

    vite configuration added for docker file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes the server accessible externally
+    port: 5173, // Ensure this matches the port in docker-compose.yml
+  }
 })
